1e46771555e95354b77ba1ebca1250f0f7a73df9
QuantumCalzone <QuantumCalzone@gmail.com>
2020-05-08 20:16:58 -0700

    import fixes

--- pythonutils/zTest.py
+++ pythonutils/zTest.py
@@ -1,6 +1,6 @@
-from .colors_utils import *
-from .os_utils import *
-from .yes_or_no_input import *
+from pythonutils.colors_utils import *
+from pythonutils.os_utils import *
+from pythonutils.yes_or_no_input import *
 
 
 def test_colors():
